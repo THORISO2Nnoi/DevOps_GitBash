662fcdbfe59a80499af01272a72ff57bd50532b0
THORISO <220674868@ump.ac.za>
2025-05-12 12:15:28 +0200

    Add Updated Java Class

---
+++ Maubane220674868.java
@@ -0,0 +1,6 @@
+public class Maubane220674868 {
+    public static void main(String[] args) {
+        System.out.println("My name is Thoriso Maubane.");
+        System.out.println("My student number is 220674868.");
+        }
+}
